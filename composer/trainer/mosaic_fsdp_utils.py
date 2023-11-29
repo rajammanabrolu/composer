dc996ffa83914c0ae74f3b25dde90fde90f1bd70
rajammanabrolu <rajammanabrolu@gmail.com>
2023-11-29 05:12:02 +0000

    partial fsdp checkpoint loading

--- composer/trainer/mosaic_fsdp_utils.py
+++ composer/trainer/mosaic_fsdp_utils.py
@@ -7,9 +7,11 @@
 """Utilities for monkey patching FSDP."""
 
 import functools
+import inspect
 import logging
 import math
 import warnings
+from functools import partial
 from typing import TYPE_CHECKING, Any, Callable, Dict, Iterable, Optional, Set, Tuple, Union, cast, no_type_check
 
 import torch
@@ -27,7 +29,7 @@ from torch.distributed.fsdp._fsdp_extensions import _ext_pre_load_state_dict_tra
 from torch.distributed.utils import _replace_by_prefix
 
 from composer.core import Precision
-from composer.utils import dist, using_torch_2
+from composer.utils import dist
 
 if TYPE_CHECKING:
     if version.parse(torch.__version__) >= version.parse('2.0.1') and version.parse(
@@ -39,10 +41,6 @@ SHARDING_MAP = {
     'SHARD_GRAD_OP': ShardingStrategy.SHARD_GRAD_OP,
     'FULL_SHARD': ShardingStrategy.FULL_SHARD,
 }
-
-if using_torch_2():
-    SHARDING_MAP['_HYBRID_SHARD_ZERO2'] = ShardingStrategy._HYBRID_SHARD_ZERO2
-    SHARDING_MAP['HYBRID_SHARD'] = ShardingStrategy.HYBRID_SHARD
 
 BACKWARD_PREFETCH_MAP = {
     'NONE': None,
@@ -200,12 +198,7 @@ def _set_custom_fsdp_module_kwargs(module_kwargs: Dict, process_group_cache: Dic
             f"Automated setting of custom per module mixed_precision is not implemented, but it can be set if `isinstance(module_kwargs['mixed_precision'], MixedPrecision)`"
         )
     if 'process_group' in module_kwargs:
-        # Call on every process group if it is a tuple
-        if isinstance(module_kwargs['process_group'], tuple):
-            module_kwargs['process_group'] = tuple(
-                _get_process_group(pg, process_group_cache) for pg in module_kwargs['process_group'])
-        else:
-            module_kwargs['process_group'] = _get_process_group(module_kwargs['process_group'], process_group_cache)
+        module_kwargs['process_group'] = _get_process_group(module_kwargs['process_group'], process_group_cache)
 
     return module_kwargs
 
